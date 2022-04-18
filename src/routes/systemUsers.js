954c15515c520c330bea780ccd8e8fb184070d5e
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2022-04-18 11:07:04 -0300

    path source and removing 2

--- src/routes/systemUsers.js
+++ src/routes/systemUsers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const login = require('../middleware/login');
 
-const SystemUserController = require('../controllers/systemusers-controller');
+const SystemUserController = require('../../controllers/systemusers-controller');
 
 router.post('/', SystemUserController.Login);
 router.post('/logout', login, SystemUserController.Logout);
